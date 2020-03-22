82fd847d8e0c6d1102bee0f3a805da423dce14a2
Cathrin Whitmer <catwhitmer@gmail.com>
2020-03-22 14:28:46 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -8,7 +8,7 @@ class Application
  
     if req.path.match(/items/)
       @@items.each do |item|
-        resp.write.item.price
+        resp.write item.price
       end
     elsif
       resp.write "Item Not Found"
